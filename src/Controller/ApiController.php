f0a734e30aa15757636cb07dc2a77175d157f9d8
Pascal Roth <pascal@truestedt.de>
2025-03-28 13:44:06 +0100

    rest api users post

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,8 +6,10 @@ use App\Entity\Users;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
@@ -26,5 +28,32 @@ class ApiController extends AbstractController
         }
 
         return $this->json($data);
+    }
+
+    #[Route('/api/users', name:'create_user', methods: ['POST'])]
+    public function createUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['email'], $data['password'], $data['role'])) {
+            return $this->json(['error' => 'Invalid input'], 400);
+        }
+
+        $user = new Users();
+        $user->setEmail($data['email']);
+        $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT));
+        $user->setRole($data['role']);
+        $user->setCreatedAt(new \DateTimeImmutable());
+
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            return new JsonResponse(['message' => 'Invalid data', 'errors' => (string) $errors], 400);
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['mesage' => 'User created successfully'], 201);
+
     }
 }
