68712c834346cd7870d417548710a4abe9c4b947
Pascal Roth <pascal@truestedt.de>
2025-03-28 23:51:46 +0100

    changed structure of controllers

--- src/Controller/ProblemController.php
+++ src/Controller/ProblemController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
-class ApiController extends AbstractController
+class ProblemController extends AbstractController
 {
     private EntityManagerInterface $entity_manager;
     private SerializerInterface $serializer;
@@ -20,42 +20,6 @@ class ApiController extends AbstractController
     public function __construct(EntityManagerInterface $entity_manager, SerializerInterface $serializer) {
         $this->entity_manager = $entity_manager;
         $this->serializer = $serializer;
-    }
-
-    #[Route('/api/users', name: 'get_users', methods: ['GET'])]
-    public function getUsers(EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
-    {
-        $users = $entityManager->getRepository(Users::class)->findAll();
-
-        $json = $serializer->serialize($users, 'json', ['groups' => 'user_read']);
-
-        return new JsonResponse($json, 200, [], true);
-    }
-
-    #[Route('/api/users', name:'create_user', methods: ['POST'])]
-    public function createUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
-        if (!isset($data['email'], $data['password'], $data['role'])) {
-            return $this->json(['error' => 'Invalid input'], 400);
-        }
-
-        $user = new Users();
-        $user->setEmail($data['email']);
-        $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT));
-        $user->setRole($data['role']);
-        $user->setCreatedAt(new \DateTimeImmutable());
-
-        $errors = $validator->validate($user);
-        if (count($errors) > 0) {
-            return new JsonResponse(['message' => 'Invalid data', 'errors' => (string) $errors], 400);
-        }
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        return new JsonResponse(['mesage' => 'User created successfully'], 201);
     }
 
     #[Route('/api/problems', name: 'get_problems', methods: ['GET'])]
@@ -68,7 +32,7 @@ class ApiController extends AbstractController
         return new JsonResponse($json,200, [], true);
     }
 
-    #[Route('/api/problems', name:'create_user', methods: ['POST'])]
+    #[Route('/api/problems', name:'create_problem', methods: ['POST'])]
     public function createProblem(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
---
+++ src/Controller/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Entity\Problems;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class UserController extends AbstractController
+{
+    private EntityManagerInterface $entity_manager;
+    private SerializerInterface $serializer;
+
+    public function __construct(EntityManagerInterface $entity_manager, SerializerInterface $serializer) {
+        $this->entity_manager = $entity_manager;
+        $this->serializer = $serializer;
+    }
+
+    #[Route('/api/users', name: 'get_users', methods: ['GET'])]
+    public function getUsers(EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $users = $entityManager->getRepository(Users::class)->findAll();
+
+        $json = $serializer->serialize($users, 'json', ['groups' => 'user_read']);
+
+        return new JsonResponse($json, 200, [], true);
+    }
+
+    #[Route('/api/users', name:'create_user', methods: ['POST'])]
+    public function createUser(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['email'], $data['password'], $data['role'])) {
+            return $this->json(['error' => 'Invalid input'], 400);
+        }
+
+        $user = new Users();
+        $user->setEmail($data['email']);
+        $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT));
+        $user->setRole($data['role']);
+        $user->setCreatedAt(new \DateTimeImmutable());
+
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            return new JsonResponse(['message' => 'Invalid data', 'errors' => (string) $errors], 400);
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['mesage' => 'User created successfully'], 201);
+    }
+}
